66d42a94ec8fdd890131f4873119106393331a92
chicken1991 <77861966+chicken1991@users.noreply.github.com>
2022-06-20 08:06:48 -0700

    append to file sorta works

--- Develop/server.js
+++ Develop/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
+const { randomUUID } = require('crypto');
 
 const PORT = 3001;
 
@@ -20,8 +22,31 @@ app.get('/notes', (req, res) =>
 
 //TODO: POST requests to write to /db/db.json
 app.post('/api/notes', (req,res) => {
-    console.log(`${req.method} request received`);
+    console.info(`${req.method} request received`);
+    
+    const { title, text } = req.body;
 
+    if (title && text) {
+        const newNote = {
+            id: randomUUID(),
+            title,
+            text,
+        };
+
+        const response = {
+            status: 'success',
+            body: newNote,
+        };
+
+        console.log(response);
+        res.status(201).json(response);
+        fs.appendFile('./db/db.json', JSON.stringify(newNote), (err) => {
+            if (err) { console.log(err); }
+            else { console.log("File written successfully\n"); }
+        });
+    } else {
+        res.status(500).json('Error in creating new note');
+    }
 })
 
 app.listen(PORT, () =>
