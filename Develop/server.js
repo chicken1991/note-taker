32b8e5057cf85e751962807f970587a2e49b5a3c
chicken1991 <77861966+chicken1991@users.noreply.github.com>
2022-06-20 20:21:12 -0700

    functional but delete is buggy

--- Develop/server.js
+++ Develop/server.js
@@ -2,7 +2,8 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const { randomUUID } = require('crypto');
-let db = require('./db/db.json');
+const { readFromFile, writeToFile } = require('./utils/fsutils.js');
+// const db = require('./db/db.json');
 
 const PORT = 3001;
 
@@ -17,13 +18,20 @@ app.get('/', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
+//directs user to notes page
 app.get('/notes', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+//GET request path for the api so that notes can be rendered
+app.get('/api/notes', (req, res) =>
+res.sendFile(path.join(__dirname, '/db/db.json'))
 );
 
 //TODO: POST requests to write to /db/db.json
 app.post('/api/notes', (req,res) => {
     console.info(`${req.method} request received`);
+    let db = require('./db/db.json');
     const { title, text } = req.body;
 
     if (title && text) {
@@ -45,10 +53,25 @@ app.post('/api/notes', (req,res) => {
             if (err) { console.log(err); }
             else { console.log("File written successfully\n"); }
         });
+        //TODO: refresh the displayed notes list
     } else {
         res.status(500).json('Error in creating new note');
     }
 })
+
+app.delete(`/api/notes/:id`, (req, res) => {
+    const noteID = req.params.id;
+    readFromFile('./db/db.json')
+    .then((data) => JSON.parse(data))
+    .then((json) => {
+        let tmpArray = json.filter((db) => db.id !== noteID);
+        console.log(tmpArray);
+
+        writeToFile('./db/db.json', tmpArray);
+
+        res.json(`Item ${noteID} has been deleted`);
+});
+});
 
 app.listen(PORT, () =>
     console.log(`App listening at localhost:${PORT}`)
